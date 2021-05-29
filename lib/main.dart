261b9f5a7d01fc8d01c1572798d0d853111f6ccd
ishti <ishtig@gmail.com>
2021-05-30 00:11:00 +0530

    Show distributed cards on 4 edges - amongst 4 players with center stack.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:least_sum/components/cards.dart' as CardComponent;
 import 'package:least_sum/components/dealer.dart';
+import 'package:stack/stack.dart' as StackDs;
 
 import 'components/shuffle.dart';
 
@@ -29,6 +30,7 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
+  final int numOfPlayers = 4;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -37,10 +39,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var cards = shuffleCards(jokersCount: 2);
   var cardsInHand = HashMap<int, List<CardComponent.Card>>();
+  var centerStack = StackDs.Stack<CardComponent.Card>();
 
   void _distributeCards() {
     setState(() {
-      cardsInHand = deal(cards, numOfPlayers: 4, numOfCards: 5);
+      cardsInHand =
+          deal(cards, numOfPlayers: widget.numOfPlayers, numOfCards: 5);
+      centerStack.push(cards.pop());
     });
   }
 
@@ -58,11 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView.builder(
-          itemCount: cardsInHand.length,
-          itemBuilder: (context, position) {
-            return Text(getStringForCardsWith(position));
-          }),
+      body: buildGameGround(),
       floatingActionButton: FloatingActionButton(
         onPressed: _distributeCards,
         tooltip: 'Increment',
@@ -71,10 +72,25 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  String getStringForCardsWith(int player) {
+  Widget buildGameGround() {
+    if (cardsInHand.length == 0) {
+      return Text('Click distribute fab to distribute cards and start game');
+    }
+    return Column(children: [
+      Text(getStringForCardsWith(0, true)),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Text(getStringForCardsWith(1, false)),
+        Text(centerStack.top().toString()),
+        Text(getStringForCardsWith(2, false))
+      ]),
+      Text(getStringForCardsWith(3, true)),
+    ]);
+  }
+
+  String getStringForCardsWith(int player, bool isHorizontal) {
     String string = '';
     for (CardComponent.Card card in cardsInHand[player]) {
-      string += ' ' + card.toString();
+      string += (isHorizontal ? ' ' : '\n') + card.toString();
     }
     return string;
   }
